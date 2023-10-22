de0e58c953b0f77eb09d32ad6c542a18fbf3db76
aminudin <amin4udin@gmail.com>
2023-10-23 01:46:12 +0700

    init

---
+++ modifyResponse.py
@@ -0,0 +1,50 @@
+# made by m1n007
+
+from burp import IBurpExtender, IHttpListener
+
+json_string = ''
+hostTarget = ""
+condition = ""
+        
+
+class BurpExtender(IBurpExtender, IHttpListener):
+    def registerExtenderCallbacks(self, callbacks):
+        extName = "Auto Modify Object Response"
+        self._callbacks = callbacks
+        self._helpers = callbacks.getHelpers()
+        
+        callbacks.registerHttpListener(self)
+        callbacks.setExtensionName(extName)
+
+        return
+
+    def getResponseHeadersAndBody(self, content):
+        request = content.getRequest()
+        request_data = self._helpers.analyzeResponse(request)
+        request_headers = list(request_data.getHeaders() or '')
+        
+        response = content.getResponse()
+        response_data = self._helpers.analyzeResponse(response)
+        headers = list(response_data.getHeaders() or '')
+        body = response[response_data.getBodyOffset():].tostring()
+        return headers, body, request_headers
+
+    def processHttpMessage(self, tool, isRequest, content):
+        if isRequest:
+            return
+        
+        print('Waiting host :', hostTarget)
+        new_headers_response, response_body, request_headers = self.getResponseHeadersAndBody(content);
+        if any(hostTarget in header for header in request_headers):
+            print('Host Found!')
+            if any("403" in header for header in new_headers_response):
+                if response_body.find(condition) != -1:
+                    print(new_headers_response)
+                    new_headers = [header.replace("403 Forbidden", "200 OK") if "403 Forbidden" in header else header for header in new_headers_response]
+                    print(new_headers)
+                    new_message = self._helpers.buildHttpMessage(new_headers, self._helpers.stringToBytes(json_string))
+                    content.setResponse(new_message)
+
+        
+        
+         
